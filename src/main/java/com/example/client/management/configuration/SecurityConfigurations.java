00a7a2fb1e658076eb8528d31edc306a568e4eda
Yuri Amorim <yuricalixto.uk@gmail.com>
2024-11-29 21:26:55 +0000

    test1

--- src/main/java/com/example/client/management/configuration/SecurityConfigurations.java
+++ src/main/java/com/example/client/management/configuration/SecurityConfigurations.java
@@ -61,13 +61,15 @@ public class SecurityConfigurations {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200", "http://localhost:4000", "http://3.85.44.28:8081", "http://3.85.44.28"));
+        configuration.setAllowedOrigins(Arrays.asList(
+            "http://localhost:4200",  // Para desenvolvimento local
+            "http://3.85.44.28"      // Para produção
+        ));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
-        configuration.setAllowedHeaders(Arrays.asList("authorization", "content-type", "x-auth-token"));
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "x-auth-token"));
         configuration.setExposedHeaders(Arrays.asList("x-auth-token"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
     }
-
 }
